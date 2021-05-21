526ea7dca4683a01dc48d8fb8aa287f6a8c8e828
JonatasDantas <jonatas.almeida14@hotmail.com>
2021-05-20 22:49:47 -0300

    heroku fixes

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ function getLocalToken() {
 }
 
 export const api = axios.create({
-  baseURL: 'http://estoque-total-api.herokuapp.com',
+  baseURL: 'https://estoque-total-api.herokuapp.com',
   headers: {
     authorization: `Bearer ${getLocalToken()}`,
   },
